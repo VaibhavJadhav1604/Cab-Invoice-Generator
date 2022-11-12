7b2355ea8bd2c40f8408c25d3cc50fbe0dee3831
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-11-12 23:23:57 +0530

    Testing File

---
+++ UnitTest3.cs
@@ -0,0 +1,19 @@
+﻿using CIG3;
+using NUnit.Framework;
+
+namespace NTest1
+{
+    public class Tests3
+    {
+
+        [Test]
+        public void Test3()
+        {
+            ride r = new ride();
+            multiplerides[] mr = { new multiplerides(20, 10), new multiplerides(10, 20) };
+            double actual = r.calculatefare(mr);
+            double expected = 330;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
